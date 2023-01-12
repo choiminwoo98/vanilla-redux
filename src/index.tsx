31b0a08319e1984a4811984f7010edeb0fa41988
choiminwoo98 <61531483+choiminwoo98@users.noreply.github.com>
2023-01-12 16:28:07 +0900

    PURE REDUX:COUNTER

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,29 @@
-<div>hello</div>;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createStore } from "redux";
 
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+let number = document.querySelector("span") as HTMLParagraphElement;
+
+const countModifier = (count: number = 0, action: any) => {
+    switch (action.type) {
+        case "ADD":
+            return count + 1;
+        case "MINUS":
+            return count - 1;
+        default:
+            return count;
+    }
+};
+const countStore = createStore(countModifier);
+const onChange = () => {
+    number.innerText = countStore.getState() + "";
+};
+countStore.subscribe(onChange);
+add?.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus?.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
 
 // const root = ReactDOM.createRoot(
 //     document.getElementById("root") as HTMLElement
